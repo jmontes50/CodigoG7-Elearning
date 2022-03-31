3a43fdb7f9b974163e67f12e07ce8e8a13246023
Osmar Montesinos <osmarmontes50@gmail.com>
2022-03-30 20:57:47 -0500

    update

---
+++ Semana5/Dia1/02-aritmetica.js
@@ -0,0 +1,26 @@
+// para variables lo recomendable es caracteres solo en inglés
+let a = 100;
+
+let b = 40;
+
+let suma = a + b;
+// cuando es texto, el + me sirve para concatenar
+console.log("esto es una suma " + suma);
+
+console.log(1000 / 200.2);
+
+let resta = a - b;
+console.log("esto es una resta " + resta);
+
+let multiplicacion = a * b;
+console.log("esto es una multiplicación " + multiplicacion);
+
+let division = a / b;
+console.log("esto es una división " + division);
+
+console.log("patatas" / 20); //NaN = Not a Number
+
+let residuo = 105 % 4;
+console.log("residuo o módulo " + residuo);
+
+console.log("porcentaje 50% " + 0.5);
