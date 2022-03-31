4fc5b0b090750ea73da45730069412a4bef6ab59
Osmar Montesinos <osmarmontes50@gmail.com>
2022-03-30 20:21:45 -0500

    update js

---
+++ Semana5/Dia1/01-introjavascript.js
@@ -0,0 +1,41 @@
+console.log("Hola mundial!");
+
+var nombre = "Osmar";
+
+//let es mucho mas seguro
+let apellido = "Montesinos";
+
+var nombre = "Jorge";
+
+apellido = "Aramayo"; //asignando un nuevo valor
+
+console.log(nombre);
+
+console.log(apellido);
+
+// tipos de datos
+
+//string, cadena de texto, nombres, palabras
+let texto = "ARRIBA PERÚ";
+//number, operaciones aritméticas
+let edad = 31;
+console.log(edad);
+//booleano - verdadero o falso , preguntar?, true o false
+let teQuiere = true;
+console.log(teQuiere);
+
+//cambiar de tipo de dato sin problemas
+//Javascript es un lenguaje que no es tipado
+let anio = 2022;
+
+anio = "dos mil veintidos";
+
+console.log(anio);
+
+let nota;
+
+console.log(nota);
+
+let nulo = null;
+
+console.log(null); //deberia haber algo pero no está
