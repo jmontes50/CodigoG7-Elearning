f070c916254ab133a0847ec750da281e3468ddad
Osmar Montesinos <osmarmontes50@gmail.com>
2022-03-31 22:01:06 -0500

    update

---
+++ Semana5/Dia2/05-while.js
@@ -0,0 +1,17 @@
+let contador = 1;
+
+/**
+ while(condicional){
+	 mientras la condicional sea verdadera
+	 se ejecutará el código dentro del while
+ }
+ */
+
+while (contador <= 15) {
+    // cualquier cantidad de codigo
+    console.log(contador <= 15);
+    console.log(contador);
+    //OJO:Necesariamente, actualizar el valor a evaluar para que no vaya al infinito
+    //contador vas a cambiar tu valor actual y aumentará en uno
+    contador = contador + 1;
+}
