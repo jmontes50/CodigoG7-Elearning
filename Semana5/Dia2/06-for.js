3d71e805dfba8145f07e131ccccbcfebddb251bf
Osmar Montesinos <osmarmontes50@gmail.com>
2022-03-31 22:24:58 -0500

    update

---
+++ Semana5/Dia2/06-for.js
@@ -0,0 +1,15 @@
+/**
+for(contador; condicional; actualizador_del_contador){
+	código que se va a repetir
+}
+ */
+//n es un número
+// a = a + n;
+// a += n;
+
+// for (let cont = 0; cont <= 10; cont = cont + 1) {
+/** 
+for (let cont = 0; cont <= 10; cont++) {
+    console.log(`el contador es ${cont}`);
+}
+*/
