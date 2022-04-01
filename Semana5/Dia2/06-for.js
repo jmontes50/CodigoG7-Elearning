89d1520c8bb584fa08de0d2819bc7281c2482508
Osmar Montesinos <osmarmontes50@gmail.com>
2022-03-31 22:35:53 -0500

    UPDATE

--- Semana5/Dia2/06-for.js
+++ Semana5/Dia2/06-for.js
@@ -13,3 +13,22 @@ for (let cont = 0; cont <= 10; cont++) {
     console.log(`el contador es ${cont}`);
 }
 */
+
+/**
+Un chico está ahorrando sus propinas para comprarse una PS5,
+cada mes el chico recibirá una propina diferente, preguntar
+ */
+
+let precioPs5 = 3000;
+
+let ahorro = 0;
+
+let propina = 0;
+
+for (ahorro = 5; ahorro <= 3000; ahorro = ahorro + propina) {
+    propina = Number(prompt("Ingrese la propina"));
+    console.log("PROPINA", propina);
+    console.log("AHORRO", ahorro);
+}
+//CUANDO SE TERMINE DE EJECUTAR EL FOR RECIÉN SE EJECUTARÁ LO QUE ESTÁ DESPUÉS
+console.log("ESTOY DESPUÉS DEL FOR");
