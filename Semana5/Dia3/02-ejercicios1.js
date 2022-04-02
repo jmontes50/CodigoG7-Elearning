51a24e160c787a6edecb2259454db146a99670d8
Osmar Montesinos <osmarmontes50@gmail.com>
2022-04-01 20:17:31 -0500

    update

---
+++ Semana5/Dia3/02-ejercicios1.js
@@ -0,0 +1,36 @@
+/**
+Uná clínica al hacer consultas va a justando los precios:
+	las 3 primeras consultas cuestan 150 c/u
+	las 2 siguientes cuestan 120 c/u
+	las 2 siguientes cuestan 100 c/u
+	y las restantes después de estas cuestan 60 c/u
+
+Se requiere saber:
+a) Cuanto pagará el paciente por todas sus citas realizadas
+ */
+
+//Primera situación, conocemos cuantas citas en total tiene el paciente.
+
+//cita 1,2,3 = 150
+//cita 4,5 = 120
+//cita 6,7 = 100
+// cita 8 a más = 60
+
+let cantidadCitas = Number(prompt("cuantas citas tuvo?"));
+
+let pagoTotal = 0;
+
+for (let i = 1; i <= cantidadCitas; i++) {
+    // if(i <= 3){
+    if (i >= 1 && i <= 3) {
+        pagoTotal = pagoTotal + 150;
+    } else if (i <= 5) {
+        pagoTotal = pagoTotal + 120;
+    } else if (i <= 7) {
+        pagoTotal = pagoTotal + 100;
+    } else {
+        pagoTotal = pagoTotal + 60;
+    }
+}
+
+console.log(`El total a pagar es: ${pagoTotal}`);
