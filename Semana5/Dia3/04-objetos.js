82861d18aa928c22cb60ecb8eebb3323b8186963
Osmar Montesinos <osmarmontes50@gmail.com>
2022-04-01 22:17:02 -0500

    update

--- Semana5/Dia3/04-objetos.js
+++ Semana5/Dia3/04-objetos.js
@@ -2,7 +2,7 @@ let gato = {
     nombre: "Tornillo",
     color: "Blanco",
     edad: 4,
-    raza: "Angora",
+    ["raza"]: "Angora",
     ["color_ojos"]: "Azul",
     hobbies: ["dormir", "jugar con monedas", "comer pollo"],
     comer: function () {
@@ -23,3 +23,41 @@ console.log(gato.color_ojos);
 console.log(gato.hobbies[1]);
 
 gato.comer();
+/** 
+function devuelveValor(arrProps) {
+    // return gato[prop];
+    let arrTemp = [];
+    for (let i = 0; i < arrProps.length; i++) {
+        arrTemp.push(gato[arrProps[i]]);
+    }
+    return arrTemp;
+}
+
+console.log("F", devuelveValor(["nombre", "edad", "color"]));
+*/
+
+let albumMusical = {
+    nombre: "Labour of Love",
+    fechaLanzamiento: "01/10/1983",
+    genero: ["Reggae", "Indie"],
+    canciones: [
+        {
+            titulo: "Red Red Wine",
+            autor: ["Neil Diamond", "UB40"],
+            duracion: null,
+        },
+        {
+            titulo: "Cherry Oh Baby",
+            autor: ["UB40"],
+            duracion: 3.18,
+        },
+    ],
+};
+
+console.log(albumMusical.canciones[0].autor[0]);
+
+// console.log(albumMusical.canciones[0].duracion + 10);
+
+if (albumMusical.canciones[0].duracion === null) {
+    console.log("NO EXISTE!");
+}
