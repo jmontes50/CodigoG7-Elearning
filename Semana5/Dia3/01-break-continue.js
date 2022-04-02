deb53763b4d53f617826af6f3f0a4ad618ab3a03
Osmar Montesinos <osmarmontes50@gmail.com>
2022-04-01 19:48:09 -0500

    update

---
+++ Semana5/Dia3/01-break-continue.js
@@ -0,0 +1,35 @@
+let alumnos = ["Javier", "Erika", "Nohemi", "Alex", "Diana"];
+
+//algoritmo para que el usuario busque un nombre y si existe el nombre dentro de alumnos me muestre su posición/índice
+
+let busqueda = prompt("Ingrese el nombre del alumnit@");
+
+for (let i = 0; i < alumnos.length; i++) {
+    console.log(alumnos[i]);
+    if (busqueda === alumnos[i]) {
+        console.log(`${busqueda} se encuentra en la posición ${i}`);
+        //corta la iteración
+        break;
+    }
+}
+
+/** 
+for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 5; j++) {
+        console.log({ i, j });
+        if (j === 3) {
+            break;
+        }
+    }
+}
+*/
+
+let notas = [15, 18, 13, 12, 9, 14, 20, 6, 0, 10];
+
+//sepa que notas son las aprobatorias
+for (let i = 0; i < notas.length; i++) {
+    if (notas[i] < 13) {
+        continue;
+    }
+    console.log(notas[i]);
+}
