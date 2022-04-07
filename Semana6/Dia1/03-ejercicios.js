6a4324a334b8957a8483bb91cb5d3277c5fee9a5
Jorge Montesinos <osmarmontes50@gmail.com>
2022-04-06 22:10:28 -0500

    update

--- Semana6/Dia1/03-ejercicios.js
+++ Semana6/Dia1/03-ejercicios.js
@@ -14,7 +14,7 @@ function peliculasPorIdioma (idioma) {
 }
 
 let pelis = peliculasPorIdioma("en")
-console.log(pelis)
+// console.log(pelis)
 
 //RETO
 //realicen una función que reciba una variable nombreGenero y me muestre las peliculas de ese genero
@@ -23,3 +23,61 @@ console.log(pelis)
 //Busquen el id del genero primero y asignen su valor a una variable
 
 //busquen en cada película con ese id
+
+/** 
+
+function peliculasPorGenero (nombreGenero) {
+    let generoEncontrado = 0;
+
+    let peliculasEncontradas = [];
+
+    for(let i = 0; i < generos.length; i++){
+        if(generos[i].name === nombreGenero){
+            generoEncontrado = generos[i].id
+            break;
+        }
+    }
+
+    for(let j = 0; j < peliculas.length; j++){
+        for(let k = 0; k < peliculas[j].genre_ids.length; k++){
+            if(peliculas[j].genre_ids[k] === generoEncontrado ){
+                peliculasEncontradas.push(peliculas[j])
+            }
+        }
+    }
+
+    return peliculasEncontradas;
+}
+
+let pelisPorGenero = peliculasPorGenero("Drama")
+
+console.log(pelisPorGenero)
+
+*/
+
+function peliculasPorGenero (nombreGenero) {
+    let generoEncontrado = 0;
+
+    let peliculasEncontradas = [];
+
+    for(let genero of generos){
+        if(genero.name === nombreGenero){
+            generoEncontrado = genero.id
+        }
+    }
+
+    console.log(generoEncontrado)
+
+    for(let peli of peliculas){
+        for(let idGenero of peli.genre_ids){
+            if(idGenero === generoEncontrado){
+                // console.log(peli)
+                peliculasEncontradas.push(peli)
+            }
+        }
+    }
+
+    return peliculasEncontradas
+}
+
+console.table(peliculasPorGenero("Drama"))
