b5a9b7c73c809587b34f8a559419a6c1771e977a
Jorge Montesinos <osmarmontes50@gmail.com>
2022-04-08 21:03:00 -0500

    update

--- Semana6/Dia3/02-submit.js
+++ Semana6/Dia3/02-submit.js
@@ -1,11 +1,35 @@
 let formulario = document.getElementById("formulario")
 let tareasguardadas = document.getElementById("tareasguardadas")
 
+let porHacer = [] //para indicar aquí las tareas que tengo por hacer
+
 formulario.addEventListener("submit", function(ev){
     ev.preventDefault()
     //formulario["name_del_input"].value me da el valor dentro del input
-    console.log(formulario["tareanueva"].value)
-    let nuevaTarea = document.createElement("p")
-    nuevaTarea.innerHTML = formulario["tareanueva"].value
-    tareasguardadas.appendChild(nuevaTarea)
+    // console.log(formulario["tareanueva"].value)
+    agregarTarea(formulario["tareanueva"].value)
 })
+
+//Un solo bloque se encargue de una sola cosa
+
+function agregarTarea (tarea) {
+    porHacer.push(tarea)
+    let nuevaTarea = document.createElement("p")
+    nuevaTarea.innerHTML = tarea
+    tareasguardadas.appendChild(nuevaTarea)
+    guardarEnLS()
+}
+
+function guardarEnLS (){
+    /** 
+     * 
+     * //localStorage solamente guarda texto
+    // localStorage.setItem("nombre",valor)
+    let fecha = {dia:08, mes:04}
+    let fechaConvertida = JSON.stringify(fecha)
+    console.log(fechaConvertida)
+    */
+    let porHacerConvertido = JSON.stringify(porHacer)
+    console.log(porHacerConvertido)
+    localStorage.setItem("tareasPorHacer", porHacerConvertido)
+}
