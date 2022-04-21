49ebaf81b2ae3b80961c92fb56542e78db4171e6
Jorge Montesinos <osmarmontes50@gmail.com>
2022-04-20 20:24:22 -0500

    update

--- Semana8/Día1/02-clases.js
+++ Semana8/Día1/02-clases.js
@@ -2,7 +2,7 @@
 //el nombre de la clase debe iniciar con Mayúscula
 
 class Pokemon {
-    //0. definir que caracteristicas va a tener mi clase
+    //0. definir que caracteristicas/propiedades va a tener mi clase
     //clase -> plano/plantilla
     //no es necesario poner let var ni nada
     Vida;
@@ -21,10 +21,17 @@ class Pokemon {
         this.Poder = poder;
         this.Tamanio = tamanio;
         this.Vida = vida;
-        this.Tipo = tipo
+        this.Tipo = tipo;
+        this.aparecer()
+    }
+
+    //no necesito ponerles function ni na
+    aparecer(){
+        console.log(`Un ${this.Nombre} salvaje apareció!`)
     }
 }
 
+//Instancia -> new 
 let ratonAmarillo = new Pokemon("Pikachu","Impact Trueno",60,20,30,"Eléctrico");
 
 let lechuguita = new Pokemon("Bulbasaur","Latigo Cepa",55, 20, 40, "Vegetal");
@@ -32,3 +39,5 @@ let lechuguita = new Pokemon("Bulbasaur","Latigo Cepa",55, 20, 40, "Vegetal");
 console.table(ratonAmarillo);
 
 console.table(lechuguita);
+
+// lechuguita.aparecer()
