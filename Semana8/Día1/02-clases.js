f3c1da595ff5f4f9c221074cad09a36df6f4bf64
Jorge Montesinos <osmarmontes50@gmail.com>
2022-04-20 20:10:51 -0500

    update

---
+++ Semana8/Día1/02-clases.js
@@ -0,0 +1,34 @@
+//class <Nombre_clase> {}
+//el nombre de la clase debe iniciar con Mayúscula
+
+class Pokemon {
+    //0. definir que caracteristicas va a tener mi clase
+    //clase -> plano/plantilla
+    //no es necesario poner let var ni nada
+    Vida;
+    Nombre;
+    Habilidad;
+    Poder;
+    Tamanio;
+    Tipo;
+    //1. Constructor. Es un método que se ejecuta al inicio de instanciar mi clase
+    //de construir el objeto
+    constructor(nombre, habilidad, poder, tamanio, vida, tipo){
+        //dentro del constructor vamos a construir el objeto
+        //this va a hacer referencia al objeto que estemos construyendo
+        this.Nombre = nombre;
+        this.Habilidad = habilidad;
+        this.Poder = poder;
+        this.Tamanio = tamanio;
+        this.Vida = vida;
+        this.Tipo = tipo
+    }
+}
+
+let ratonAmarillo = new Pokemon("Pikachu","Impact Trueno",60,20,30,"Eléctrico");
+
+let lechuguita = new Pokemon("Bulbasaur","Latigo Cepa",55, 20, 40, "Vegetal");
+
+console.table(ratonAmarillo);
+
+console.table(lechuguita);
