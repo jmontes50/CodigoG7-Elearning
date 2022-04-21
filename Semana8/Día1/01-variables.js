8c8e44894e92a4293cee12e0f17eca2e623e4ab3
Jorge Montesinos <osmarmontes50@gmail.com>
2022-04-20 19:37:01 -0500

    update

---
+++ Semana8/Día1/01-variables.js
@@ -0,0 +1,14 @@
+var manzana = "apple"
+
+var manzana = "Manzana Peruanita"
+
+console.log(manzana)
+
+//var casi no tiene limitación de ambito/scope
+
+for(let i = 0; i < 3; i ++){
+    console.log(i)
+}
+
+// console.log(`Iterador: ${i}`)
+
