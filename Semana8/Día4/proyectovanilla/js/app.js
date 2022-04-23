036faa7bcf1cdad349d981690739f0f8bba5f18b
Jorge Montesinos <osmarmontes50@gmail.com>
2022-04-23 15:49:25 -0500

    update

--- Semana8/Día4/proyectovanilla/js/app.js
+++ Semana8/Día4/proyectovanilla/js/app.js
@@ -4,3 +4,12 @@
 1.1 Esos datos estan en el formato o estructura que necesitO (MOCKAPI, guarda un JSON)
 2. Convertir esos datos e insertarlos en el HTML
  */
+
+import { getProducts } from "./productoService.js";
+
+const obtainProducts = async () => {
+    const products = await getProducts()
+    console.table(products)
+}
+
+obtainProducts();
