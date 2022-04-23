f3f6f4e5f89ac8847a3e31972ff40131c324aaa7
Jorge Montesinos <osmarmontes50@gmail.com>
2022-04-22 19:53:46 -0500

    update

--- Semana8/Día3/01-asyncawait.js
+++ Semana8/Día3/01-asyncawait.js
@@ -9,10 +9,22 @@ const traerDatos = async () => {
     throw "Error al obtener los datos" //reject - mal
 }
 
-traerDatos()
-.then((rpta) => {
-    console.log(rpta)
-})
-.catch((error) => {
-    console.log(error)
-})
+// traerDatos()
+// .then((rpta) => {
+//     console.log(rpta)
+// })
+// .catch((error) => {
+//     console.log(error)
+// })
+
+const pedirDatos = async () => {
+    //await = Wait Chepiiiii, espera, esto es asíncrono
+    try {
+        //ejecuta este bloque de codigo y si todo va bien dame el resultado .then() / return / resolve
+        let rpta = await traerDatos(); 
+        console.log(rpta)
+    } catch (errorcito) { //.catch() //throw / reject
+        console.log(errorcito)
+    }
+}
+pedirDatos()
