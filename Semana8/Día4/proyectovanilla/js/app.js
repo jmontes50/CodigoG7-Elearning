b36b8adca0c18fc777dad41a4557be2075383e9b
Jorge Montesinos <osmarmontes50@gmail.com>
2022-04-27 22:38:47 -0500

    update

--- Semana8/Día4/proyectovanilla/js/app.js
+++ Semana8/Día4/proyectovanilla/js/app.js
@@ -5,7 +5,7 @@
 2. Convertir esos datos e insertarlos en el HTML
  */
 
-import { getProducts } from "./productoService.js";
+import { getProducts, createProduct } from "./productoService.js";
 import { drawProducts } from "./interfaz.js";
 //necesitamos tres referencias, 01 para el botón para escuchar el click!
 //para el modal para transformarlo en un modal de bootstrap
@@ -32,10 +32,12 @@ const obtainProducts = async () => {
 obtainProducts();
 
 btnCrear.addEventListener("click", () => {
-    bsModalCrear.show()
+    bsModalCrear.show() //muestra
 })
 
-formCrear.addEventListener("submit", (e) => {
+//vamos a indicar que la función que va a ejecutar el submit va a ser asincrona porque tenemos que esperar la 
+//respuesta del fetch
+formCrear.addEventListener("submit", async (e) => {
     //e = evento, preventDefault
     e.preventDefault();
     //obtener los valores de los input
@@ -45,5 +47,15 @@ formCrear.addEventListener("submit", (e) => {
         prod_desc:formCrear.prod_desc.value,
         prod_price:formCrear.prod_price.value,
     }
-    console.log(newProduct)
+    // console.log(newProduct)
+    try {
+        //tuvimos éxito, GO O NO GO?  dijimos GO
+        await createProduct(newProduct)
+        formCrear.reset() //limpia los input
+        bsModalCrear.hide() //escondemos el modal
+        obtainProducts() //obtenemos una actualización de los productos
+        //si llamamos sin el await, lo ejecuta pero no capturaremos ninguna respuesta de esa función
+    } catch (error) {
+        alert("Hubo un error al crear!")
+    }
 })
