a9c448e8cc247d7b465cd17ee6847341b281e11a
Jorge Montesinos <osmarmontes50@gmail.com>
2022-04-28 23:02:44 -0500

    update

--- Semana9/Día2/introreact/src/App.js
+++ Semana9/Día2/introreact/src/App.js
@@ -1,12 +1,37 @@
+import {useState} from "react"
 //1. los nombres de los componentes tienen que comenzar con Mayúscula
 const App = () => {
   //2. todo los archivos que utilicemos para la aplicación tienen que estar dentro de la carpeta src
   //3. todo componente debe tener un return
   //3.1 dentro de ese return ira el JSX, React utiliza para los return que contengan JSX el símbolo de paréntesis ()
+  //4. el return solo puede retornar 01 sola etiqueta
+
+  //los estados de un componente son INMUTABLES es que no se pueden modificar
+  //let [estado, funciónQueActualizaElEstado] = useState(estadoInicial)
+  let [contador, setContador] = useState(0)
+
+  let miTexto = "Hola desde Javascript!!!";
+  //5. cuando deseemos incluir código de JS dentro de JSX tenemos que utilizar {}
+
+  const saludar = () => {
+    return "Holaaaaa desde una función"
+  }
+
   return (
-    <div>
-      <p>Hola Mundo!</p>
-    </div>
+    <>
+      <div>
+        <p>Hola Mundo!</p>
+      </div>
+      <div>
+        <p>{miTexto}</p>
+        <p>{10 + 20}</p>
+        <p>{saludar()}</p>
+        <p>Contador: {contador}</p>
+      </div>
+      <button onClick={() => {setContador(contador ++)}}>
+        Aumentar!
+      </button>
+    </>
   )
 
 }
