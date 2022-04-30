62d3e402e92716432d33575ae68d1a2ca5e500bf
Jorge Montesinos <osmarmontes50@gmail.com>
2022-04-29 23:01:29 -0500

    update

--- Semana9/Día2/todo/src/App.js
+++ Semana9/Día2/todo/src/App.js
@@ -3,6 +3,8 @@ import { useState } from "react"
 export default function App() {
   //const [estado, funcSetEstado] = useState(estadoInicial)
   const [tareas, setTareas] = useState(["Lavar la Ropa","Comprar Té"])
+  const [nuevaTarea, setNuevaTarea] = useState("mi tarea")
+  //Componentes controlados, todo value de un input debe estar amarrado a un estado
 
   //si una funcion retorna con () dentro de react, react asume que el return es JSX
   return (
@@ -10,10 +12,17 @@ export default function App() {
       <h1>ToDo App</h1>
       <ul>
         {/**renderizado de listas */}
-        {tareas.map((item) => (
-          <li>{item}</li>
+        {/**Cuando hacemos renderizado de listas hay que añadir a c/elemento un key y lo común es darle el indice(posición) de c/elemento en el arreglo */}
+        {tareas.map((item, i) => (
+          <li key={i}>{item}</li>
         ))}
       </ul>
+      <input
+        type="text"
+        placeholder="Lavar la ropa"
+        value={nuevaTarea}
+        onChange={(e) => {setNuevaTarea(e.target.value)}}
+      />
     </div>
   )
 }
