ceba4139db2c80d65f60f4487caddc945c80a546
Jorge Montesinos <osmarmontes50@gmail.com>
2022-04-30 17:26:53 -0500

    UPDATE

--- Semana9/Día2/todo/src/App.js
+++ Semana9/Día2/todo/src/App.js
@@ -1,18 +1,22 @@
 import { useState } from "react";
 import TareaComponent from "./components/TareaComponent";
+import InputTareaComponent from "./components/InputTareaComponent";
 
 export default function App() {
-    const [tareas, setTareas] = useState(["bañar al perro"])
-    const [nuevaTarea, setNuevaTarea] = useState("")
+  const [tareas, setTareas] = useState(["bañar al perro"]);
+  const [nuevaTarea, setNuevaTarea] = useState("");
   return (
-      <div>
-          <h1>ToDo App</h1>
-          {tareas.map((item, i) => (
-                <TareaComponent 
-                    key={i}
-                    tarea={item} 
-                />
-          ))}
-      </div>
+    <div>
+      <h1>ToDo App</h1>
+
+      {tareas.map((item, i) => (
+        <TareaComponent key={i} tarea={item} />
+      ))}
+      
+      <InputTareaComponent
+        nuevaTarea={nuevaTarea}
+        setNuevaTarea={setNuevaTarea}
+      />
+    </div>
   );
 }
