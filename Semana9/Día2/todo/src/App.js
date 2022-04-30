c81a7fd97d27317484aea39c91234249b15f9844
Jorge Montesinos <osmarmontes50@gmail.com>
2022-04-30 16:53:39 -0500

    update

--- Semana9/Día2/todo/src/App.js
+++ Semana9/Día2/todo/src/App.js
@@ -1,6 +1,18 @@
+import { useState } from "react";
+import TareaComponent from "./components/TareaComponent";
 
 export default function App() {
+    const [tareas, setTareas] = useState(["bañar al perro"])
+    const [nuevaTarea, setNuevaTarea] = useState("")
   return (
-    <div>App</div>
-  )
+      <div>
+          <h1>ToDo App</h1>
+          {tareas.map((item, i) => (
+                <TareaComponent 
+                    key={i}
+                    tarea={item} 
+                />
+          ))}
+      </div>
+  );
 }
---
+++ Semana9/Día2/todo/src/components/TareaComponent.js
@@ -0,0 +1,9 @@
+export default function TareaComponent({tarea}) {
+    // console.log(props)
+  return (
+    <li>
+        <span>{tarea}</span>
+        <button>Eliminar</button>
+    </li>
+  )
+}
