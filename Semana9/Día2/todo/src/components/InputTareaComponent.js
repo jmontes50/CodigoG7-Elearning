f28591dd176b96065f19a16be90b308cf613888a
Jorge Montesinos <osmarmontes50@gmail.com>
2022-04-30 18:11:15 -0500

    update

--- Semana9/Día2/todo/src/components/InputTareaComponent.js
+++ Semana9/Día2/todo/src/components/InputTareaComponent.js
@@ -1,5 +1,5 @@
 
-export default function InputTareaComponent({nuevaTarea, setNuevaTarea}) {
+export default function InputTareaComponent({nuevaTarea, setNuevaTarea, agregarTarea}) {
 
     const manejarInput = (e) => {
         setNuevaTarea(e.target.value)
@@ -12,6 +12,11 @@ export default function InputTareaComponent({nuevaTarea, setNuevaTarea}) {
             value={nuevaTarea}
             onChange={(e) => {manejarInput(e)}}
         />
+        <button
+          onClick={agregarTarea}
+        >
+          Agregar!
+        </button>
     </div>
   )
 }
