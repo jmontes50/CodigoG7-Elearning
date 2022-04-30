c50b0fd286f66629bf4468b53eae82f1d47b2d47
Jorge Montesinos <osmarmontes50@gmail.com>
2022-04-30 15:58:51 -0500

    update

--- Semana9/Día2/todo/src/App.js
+++ Semana9/Día2/todo/src/App.js
@@ -2,9 +2,17 @@ import { useState } from "react"
 
 export default function App() {
   //const [estado, funcSetEstado] = useState(estadoInicial)
-  const [tareas, setTareas] = useState(["Lavar la Ropa","Comprar Té"])
+  const [tareas, setTareas] = useState(["Lavar la Ropa","Comprar Té","Juntarse con amigos"])
   const [nuevaTarea, setNuevaTarea] = useState("mi tarea")
   //Componentes controlados, todo value de un input debe estar amarrado a un estado
+
+  const agregarTarea = () => {
+    // let tareasTmp = [...tareas]
+    // console.log({tareasTmp})
+    // tareasTmp.push(nuevaTarea)
+    // setTareas(tareasTmp)
+    setTareas([...tareas, nuevaTarea])
+  }
 
   //si una funcion retorna con () dentro de react, react asume que el return es JSX
   return (
@@ -17,12 +25,17 @@ export default function App() {
           <li key={i}>{item}</li>
         ))}
       </ul>
+      {/* componentes controlados */}
       <input
         type="text"
         placeholder="Lavar la ropa"
         value={nuevaTarea}
         onChange={(e) => {setNuevaTarea(e.target.value)}}
       />
+      {/* eventos, onNombreDelEvento={func a ejecutar} */}
+      <button onClick={() => {agregarTarea()}}>
+        Añadir Tarea
+      </button>
     </div>
   )
-}
+} 
