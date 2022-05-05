0b6da7866378f8aed66fc91c0f5ba04745ead7f2
Jorge Montesinos <osmarmontes50@gmail.com>
2022-05-04 21:02:15 -0500

    update

--- Semana10/admin/src/views/DashboardView.js
+++ Semana10/admin/src/views/DashboardView.js
@@ -1,6 +1,14 @@
-
+import { useState, useEffect } from "react"
+import { obtenerCategorias } from "../services/categoriasService"
 
 export default function DashboardView() {
+  const [categorias, setCategorias] = useState([]);
+
+  useEffect(() => {
+    obtenerCategorias()
+  })
+
+
   return (
     <div>DashboardView</div>
   )
