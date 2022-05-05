e128c6cc735d56e6e9b96a594c58b405a0348d4c
Jorge Montesinos <osmarmontes50@gmail.com>
2022-05-04 20:37:15 -0500

    update

--- Semana10/admin/src/services/categoriasService.js
+++ Semana10/admin/src/services/categoriasService.js
@@ -1,4 +1,20 @@
 //peticiones - axios
 import axios from "axios";
 
-const URL = "https://62729d5925fed8fcb5f724b1.mockapi.io/categorias"
+const URL = "https://62729d5925fed8fcb5f724b1.mockapi.io/categorias";
+
+const obtenerCategorias = () => {
+    return new Promise ((resolve, reject) => {
+        axios.get(URL)
+        .then((rpta) => {
+            console.log(rpta)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    })
+}
+
+export {
+    obtenerCategorias
+}
