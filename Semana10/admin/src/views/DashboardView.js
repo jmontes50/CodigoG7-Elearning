af2b91b6cfec8fa024a9238c827baeb7413cd342
Jorge Montesinos <osmarmontes50@gmail.com>
2022-05-04 21:22:36 -0500

    update

--- Semana10/admin/src/views/DashboardView.js
+++ Semana10/admin/src/views/DashboardView.js
@@ -6,7 +6,11 @@ export default function DashboardView() {
 
   useEffect(() => {
     obtenerCategorias()
-  })
+    .then((data) => {
+      setCategorias(data)
+    })
+    //si le damos como argumento al useEffect un arreglo vacio como 2do parámetro, el useEffect solo se ejecutará una vez en la fase de montaje
+  }, [])
 
 
   return (
