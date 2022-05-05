aef60c01274b26db9dca49da64e27beb4d1c4c77
Jorge Montesinos <osmarmontes50@gmail.com>
2022-05-04 21:14:12 -0500

    update

--- Semana10/admin/src/services/categoriasService.js
+++ Semana10/admin/src/services/categoriasService.js
@@ -6,11 +6,16 @@ const URL = "https://62729d5925fed8fcb5f724b1.mockapi.io/categorias";
 const obtenerCategorias = () => {
     return new Promise ((resolve, reject) => {
         axios.get(URL)
-        .then((rpta) => {
-            console.log(rpta)
+        .then(({data, status}) => {
+            //valido el status - ok
+            if(status === 200){
+                resolve(data)
+            }else{
+                reject("Error al obtener data")
+            }
         })
         .catch((error) => {
-            console.log(error)
+            reject(error)
         })
     })
 }
