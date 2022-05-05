406f0e767c076e2cf91829f4253dd454feb82664
Jorge Montesinos <osmarmontes50@gmail.com>
2022-05-04 22:48:30 -0500

    update

--- Semana10/admin/src/views/CrearCategoriaView.js
+++ Semana10/admin/src/views/CrearCategoriaView.js
@@ -1,5 +1,29 @@
 export default function CrearCategoriaView() {
   return (
-    <div>CrearCategoriaView</div>
+    <>
+        <h1>Crear Categoria</h1>
+        <form>
+            <div className="mb-3">
+                <label className="form-label">
+                    Nombre categoria
+                </label>
+                <input 
+                    className="form-control"
+                    type="text"
+                    placeholder="Ej. Cafés"
+                />
+            </div>
+            <div className="mb-3">
+                <label className="form-label">
+                    Descripción categoria
+                </label>
+                <input 
+                    className="form-control"
+                    type="text"
+                    placeholder="Indique una descripción larga de la categoria"
+                />
+            </div>
+        </form>
+    </>
   )
 }
