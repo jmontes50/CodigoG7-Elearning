a9f37203d838056da16fb6d7adc643794a80e1fb
Jorge Montesinos <osmarmontes50@gmail.com>
2022-05-05 22:03:06 -0500

    update

--- Semana10/admin/src/components/Navigation.js
+++ Semana10/admin/src/components/Navigation.js
@@ -1,4 +1,5 @@
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom"
 
 export default function Navigation() {
   return (
@@ -7,6 +8,20 @@ export default function Navigation() {
         <Navbar.Brand>
             Admin App
         </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar" />
+        <Navbar.Collapse id="navbar">1
+          <Nav className="me-auto">
+            <Link className="nav-link" to="/">
+              Dashboard
+            </Link>
+            <Link className="nav-link" to="/crearcategoria">
+              Crear Categoria
+            </Link>
+            <Link className="nav-link" to="/crearlugar">
+              Crear Lugar
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
---
+++ Semana10/admin/src/views/LugaresView.js
@@ -0,0 +1,7 @@
+
+
+export default function LugaresView() {
+  return (
+    <div>LugaresView</div>
+  )
+}
