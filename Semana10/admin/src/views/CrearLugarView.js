1bc39c25e554a847a34f1e7acfd1294ece66fe5e
Osmar Montesinos <osmarmontes50@gmail.com>
2022-05-11 19:32:09 -0500

    update

--- Semana10/admin/src/views/CrearLugarView.js
+++ Semana10/admin/src/views/CrearLugarView.js
@@ -1,145 +1,158 @@
-import { useState, useEffect } from 'react'
-import { obtenerCategorias } from '../services/categoriasService'
-import { crearLugar } from '../services/lugaresService'
-import { useNavigate } from 'react-router-dom'
-import Swal from 'sweetalert2'
-import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet"
+import { useState, useEffect } from "react";
+import { obtenerCategorias } from "../services/categoriasService";
+import { crearLugar } from "../services/lugaresService";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  useMapEvents,
+  Popup,
+} from "react-leaflet";
 
 export default function CrearLugarView() {
   const [inputs, setInputs] = useState({
-    lug_nom:"",
-    lug_desc:"",
-    lug_dir:"",
-    categoriaId:1
-  })
-  const [categorias, setCategorias] = useState([])
+    lug_nom: "",
+    lug_desc: "",
+    lug_dir: "",
+    categoriaId: 1,
+    lug_coords:[-12.018, -77.005]
+  });
+  const [categorias, setCategorias] = useState([]);
   //[latitud, longitud]
-  const [marcador, setMarcador] = useState([-12, -77])
+  // const [marcador, setMarcador] = useState([]);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const manejarInputs = (e) => {
-    console.log(e.target.name)
+    console.log(e.target.name);
     setInputs({
       ...inputs, //spread operator de las propiedades de inputs
       //era segun el name que recibiamos a partir del evento,
       //le asignabamos el valor
-      [e.target.name]:e.target.value
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
-  const manejarSubmit = async(e) => {
-    e.preventDefault()
+  const manejarSubmit = async (e) => {
+    e.preventDefault();
     try {
-      await crearLugar(inputs)
+      await crearLugar(inputs);
       Swal.fire({
-        icon:"success",
-        title:"Lugar creado!"
-      })
-      navigate("/lugares")
-
+        icon: "success",
+        title: "Lugar creado!",
+      });
+      navigate("/lugares");
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   const existeErrorInputs = () => {
-    if(inputs.lug_nom.trim() === "" || 
-    inputs.lug_desc.trim() === "" || 
-    inputs.lug_dir.trim() === ""){
+    if (
+      inputs.lug_nom.trim() === "" ||
+      inputs.lug_desc.trim() === "" ||
+      inputs.lug_dir.trim() === ""
+    ) {
       //si alguno de los inputs quitandoles los espacios no tiene nada de texto retornare true
       //Si se ejecuta un return ahí finaliza la función
-      return true
+      return true;
     }
-    return false
-  }
+    return false;
+  };
 
   const AddMarker = () => {
     const map = useMapEvents({
       click: (e) => {
-        console.log(e)
-        const {lat, lng} = e.latlng
-        setMarcador([lat, lng])
-      }
-    })
-  }
+        console.log(e);
+        const { lat, lng } = e.latlng;
+        setInputs({
+          ...inputs,
+          lug_coords:[lat, lng]
+        });
+      },
+    });
+  };
 
   useEffect(() => {
     const getCategorias = async () => {
       try {
-        const categoriasObtenidas = await obtenerCategorias()
+        const categoriasObtenidas = await obtenerCategorias();
         // console.log({categoriasObtenidas})
         //solamente necesitamos el id y el nombre
-        const infoCategorias = categoriasObtenidas.map(({cat_id, cat_nom}) => {
-          return {cat_nom:cat_nom, cat_id:cat_id}
-        })
+        const infoCategorias = categoriasObtenidas.map(
+          ({ cat_id, cat_nom }) => {
+            return { cat_nom: cat_nom, cat_id: cat_id };
+          }
+        );
         // console.log({infoCategorias})
-        setCategorias(infoCategorias)
+        setCategorias(infoCategorias);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-    }
-    getCategorias()
-  }, [])
+    };
+    getCategorias();
+  }, []);
 
   return (
     <div>
-      <h1 className='mb-3'>
-        Crear Lugar
-      </h1>
-      <form onSubmit={(e) => {manejarSubmit(e)}}>
-        <div className='mb-3'>
-          <label className='form-label'>
-            Nombre lugar:
-          </label>
-          <input 
+      <h1 className="mb-3">Crear Lugar</h1>
+      <form
+        onSubmit={(e) => {
+          manejarSubmit(e);
+        }}
+      >
+        <div className="mb-3">
+          <label className="form-label">Nombre lugar:</label>
+          <input
             type="text"
             placeholder="Ej. Cevicheria La Casita"
-            className='form-control'
+            className="form-control"
             name="lug_nom"
             value={inputs.lug_nom}
-            onChange={(e) => {manejarInputs(e)}}
+            onChange={(e) => {
+              manejarInputs(e);
+            }}
           />
         </div>
 
-        <div className='mb-3'>
-          <label className='form-label'>
-            Descripción del lugar
-          </label>
-          <input 
+        <div className="mb-3">
+          <label className="form-label">Descripción del lugar</label>
+          <input
             type="text"
             placeholder="Ingrese descripción"
             className="form-control"
             name="lug_desc"
             value={inputs.lug_desc}
-            onChange={(e) => {manejarInputs(e)}}
+            onChange={(e) => {
+              manejarInputs(e);
+            }}
           />
         </div>
-        <div className='mb-3'>
-          <label className='form-label'>
-            Dirección del lugar
-          </label>
-          <input 
+        <div className="mb-3">
+          <label className="form-label">Dirección del lugar</label>
+          <input
             type="text"
             placeholder="Ingrese dirección"
             className="form-control"
             name="lug_dir"
             value={inputs.lug_dir}
-            onChange={(e) => {manejarInputs(e)}}
+            onChange={(e) => {
+              manejarInputs(e);
+            }}
           />
         </div>
 
-        <div className='mb-3'>
-          <label className='form-label'>
-            Seleccione una categoría
-          </label>
-          <select 
-            className='form-select'
+        <div className="mb-3">
+          <label className="form-label">Seleccione una categoría</label>
+          <select
+            className="form-select"
             name="categoriaId"
             value={inputs.categoriaId}
             onChange={(e) => manejarInputs(e)}
           >
-            {categorias.map(({cat_id, cat_nom}, i) => (
+            {categorias.map(({ cat_id, cat_nom }, i) => (
               <option value={cat_id} key={i}>
                 {cat_nom}
               </option>
@@ -147,10 +160,10 @@ export default function CrearLugarView() {
           </select>
         </div>
         {/* estilos directamente, style={{propiedad:"valor"}}  */}
-        <MapContainer
-          center={[-12, -77]}
-          zoom={15}
-          style={{height:"400px"}}
+        <MapContainer 
+          center={inputs.lug_coords} 
+          zoom={15} 
+          style={{ height: "400px" }}
         >
           {/* proveedor de datos para el mapa */}
           <TileLayer
@@ -158,16 +171,20 @@ export default function CrearLugarView() {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           <AddMarker />
-          <Marker position={marcador} />
+          <Marker position={inputs.lug_coords}>
+            <Popup>
+              Información del lugar {inputs.lug_nom}
+            </Popup>
+          </Marker>
         </MapContainer>
-        <button 
-          className='btn btn-primary' 
-          type="submit" 
+        <button
+          className="btn btn-primary"
+          type="submit"
           disabled={existeErrorInputs()}
         >
           Guardar
         </button>
       </form>
     </div>
-  )
+  );
 }
