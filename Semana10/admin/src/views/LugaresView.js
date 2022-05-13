931f1c586709a27c0bf52c559fa74a89371a48ee
Osmar Montesinos <osmarmontes50@gmail.com>
2022-05-12 23:04:10 -0500

    update

--- Semana10/admin/src/views/LugaresView.js
+++ Semana10/admin/src/views/LugaresView.js
@@ -1,26 +1,50 @@
 import { useState, useEffect } from "react"
 import { obtenerCategorias } from "../services/categoriasService"
 import { Link } from "react-router-dom"
+import { eliminarLugar } from "../services/lugaresService"
+import Swal from "sweetalert2"
 
 export default function LugaresView() {
 
   const [lugares, setLugares] = useState([])
 
-  useEffect(() => {
-    const getCategorias = async () => {
-      try {
-        const categorias = await obtenerCategorias()
-        // console.log(categorias)
-        //1. he filtrado solo las categorias que tengan al menos 01 lugar -> lugares.length
-        const catFiltradas = categorias.filter((cat) => cat.lugares.length > 0)
-        // console.log("filtrado", catFiltradas)
-        //2. al tener esas categorias filtradas solamente me quedo con su propiedad lugares y el flat para que todo este en 01 solo arreglo
-        const arrLugares = catFiltradas.map((cat) => cat.lugares).flat()
-        setLugares(arrLugares)
-      } catch (error) {
-        console.log(error)
+  const deleteLugar = async (idCat, idLugar) => {
+    try {
+      const resultado = await Swal.fire({
+        title: 'Desea eliminar este Lugar',
+        showCancelButton: true,
+        confirmButtonText: 'Si, eliminar',
+        cancelButtonText: `No, cancelar`,
+      })
+      if(resultado.isConfirmed){
+        await eliminarLugar(idCat, idLugar)
+        Swal.fire({
+          title:"Lugar eliminado",
+          icon:"success"
+        })
+        getCategorias()
       }
+    } catch (error) {
+      console.log(error)
     }
+  }
+
+  const getCategorias = async () => {
+    try {
+      const categorias = await obtenerCategorias()
+      // console.log(categorias)
+      //1. he filtrado solo las categorias que tengan al menos 01 lugar -> lugares.length
+      const catFiltradas = categorias.filter((cat) => cat.lugares.length > 0)
+      // console.log("filtrado", catFiltradas)
+      //2. al tener esas categorias filtradas solamente me quedo con su propiedad lugares y el flat para que todo este en 01 solo arreglo
+      const arrLugares = catFiltradas.map((cat) => cat.lugares).flat()
+      setLugares(arrLugares)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
     getCategorias();
   }, [])
   
@@ -52,6 +76,13 @@ export default function LugaresView() {
                 <Link className="btn btn-warning btn-sm" to={`/editarlugar/${categoriaId}/${lug_id}`}>
                   <i className="fa-solid fa-file-pen" />
                 </Link>
+
+                <button 
+                  className="btn btn-danger btn-sm ms-2" 
+                  onClick={() => {deleteLugar(categoriaId, lug_id)}}
+                >
+                  <i className="fa-solid fa-trash" />
+                </button>
               </td>
             </tr>
           ))}
