8707255678ea3252aea79d125e9f316783612b89
Osmar Montesinos <osmarmontes50@gmail.com>
2022-05-13 22:14:57 -0500

    update

--- Semana11/descubre/src/App.js
+++ Semana11/descubre/src/App.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import HomeView from "./views/HomeView"
+import DetalleLugarView from "./views/DetalleLugarView"
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <div className="container mt-4">
         <Routes>
           <Route path="/" element={<HomeView />} />
+          <Route path="/detallelugar/:catId/:lugId" element={<DetalleLugarView />} />
         </Routes>
       </div>
     </Router>
--- Semana11/descubre/src/views/DetalleLugarView.js
+++ Semana11/descubre/src/views/DetalleLugarView.js
@@ -1,9 +1,24 @@
+import { useState, useEffect} from "react"
 import { useParams } from "react-router-dom"
+import { obtenerLugar } from "../services/lugarService"
 
 export default function DetalleLugarView() {
   
+  const [miLugar, setMiLugar] = useState(null)
   const { catId, lugId } = useParams()
-  console.log({ catId, lugId })
+
+  useEffect(() => {
+    const getLugar = async () => {
+      try {
+        const lugObtenido = await obtenerLugar(catId, lugId)
+        setMiLugar(lugObtenido)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getLugar()
+  }, [])
+
   return (
     <div>DetalleLugarView</div>
   )
