7c8bd07243b58314d795db635342c747dbc13b73
Osmar Montesinos <osmarmontes50@gmail.com>
2022-05-13 20:29:31 -0500

    UPDATE

--- Semana11/descubre/src/components/CategoriasList.js
+++ Semana11/descubre/src/components/CategoriasList.js
@@ -2,21 +2,37 @@ import { useState, useEffect } from "react";
 import { obtenerCategorias } from "../services/categoriaService";
 
 export default function CategoriasList() {
-
-  const [misCategorias, setMisCategorias] = useState([])
+  const [misCategorias, setMisCategorias] = useState([]);
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(10);
 
   const getCategorias = async () => {
     try {
-      const catObtenidas = await obtenerCategorias()
-      console.log(catObtenidas)
+      const catObtenidas = await obtenerCategorias(page, limit);
+      setMisCategorias([...misCategorias, ...catObtenidas]);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
+  };
+
+  const manejarVerMas = () => {
+    setPage(page + 1)
   }
 
   useEffect(() => {
-    getCategorias()
-  }, [])
-  
-  return <div>CategoriasList</div>;
+    getCategorias();
+  }, [page]);
+
+  return (
+    <div>
+      {misCategorias.map(({ cat_nom }, i) => (
+        <button className="btn btn-success btn-sm me-2" key={i}>
+          {cat_nom}
+        </button>
+      ))}
+      <button className="btn btn-outline-success btn-sm" onClick={manejarVerMas}>
+        Ver más...
+      </button>
+    </div>
+  );
 }
