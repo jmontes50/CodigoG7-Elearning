8f2767ad0e3bd43478c93ee34016fc86fdee0257
Osmar Montesinos <osmarmontes50@gmail.com>
2022-05-13 20:12:36 -0500

    update

--- Semana11/descubre/src/services/categoriaService.js
+++ Semana11/descubre/src/services/categoriaService.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 const URL = process.env.REACT_APP_API;
 
-const obtenerCategorias = async () => {
+const obtenerCategorias = async (page = 1, limit = 10) => {
     try {
-        const { data, status } = await axios.get(`${URL}/categorias`);
+        //url.com?arg1=valor_arg1&arg2=valor_arg2&arg3=valor_Arg3
+        const { data, status } = await axios.get(`${URL}/categorias?page=${page}&limit=${limit}`);
         if(status === 200) return data
         throw Error("Error al obtener data")
     } catch (error) {
