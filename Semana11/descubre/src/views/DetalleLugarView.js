5a6f79fd6740dcbe1841b6aa72d5b99dfacbf72c
Osmar Montesinos <osmarmontes50@gmail.com>
2022-05-13 22:59:50 -0500

    update

--- Semana11/descubre/src/views/DetalleLugarView.js
+++ Semana11/descubre/src/views/DetalleLugarView.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { obtenerLugar } from "../services/lugarService";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 export default function DetalleLugarView() {
   const [miLugar, setMiLugar] = useState(null);
@@ -20,7 +21,7 @@ export default function DetalleLugarView() {
 
   return (
     <>
-    {/* puede pasar que al querer renderizar a partir de un objeto, una variable, un estado, un props
+      {/* puede pasar que al querer renderizar a partir de un objeto, una variable, un estado, un props
       Esa referencia no tenga una propiedad que uds busquen, ahí preguntamos si existe con un operador ternario
       y renderizamos una cosa u otra
     */}
@@ -33,7 +34,28 @@ export default function DetalleLugarView() {
                 <i className="fa-solid fa-location-dot me-2 text-success"></i>
                 {miLugar.lug_dir}
               </p>
+              <div className="img-fluid">
+                <img src={miLugar.lug_img} alt={miLugar.lug_nom} />
+              </div>
             </div>
+            <p className="lead mt-3">{miLugar.lug_desc}</p>
+            <MapContainer 
+              style={{ height: "500px" }}
+              center={miLugar.lug_coords}
+              zoom={18}
+            >
+               <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={miLugar.lug_coords}>
+                  <Popup>
+                    <i className="fa-solid fa-location-arrow text-success me-2 fa-2x"></i>
+                    <h6>{miLugar.lug_nom}</h6>
+                    <small>{miLugar.lug_desc}</small>
+                  </Popup>
+                </Marker>
+            </MapContainer>
           </div>
         </div>
       ) : (
