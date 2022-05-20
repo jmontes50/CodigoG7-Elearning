aab1f797bb7a129c515ba63992855f65d5b7f2f8
Osmar Montesinos <osmarmontes50@gmail.com>
2022-05-19 22:13:19 -0500

    update

--- Semana11/descubre/src/components/BarNavigation.js
+++ Semana11/descubre/src/components/BarNavigation.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom"
 import Badge from '@mui/material/Badge';
 import { FavoritosContext } from "../context/favoritosContext";
@@ -21,17 +21,37 @@ export default function BarNavigation() {
             <Link className="nav-link" to="/">
               Inicio
             </Link>
-            <Link className="nav-link" to="/favoritos">
-              <Badge badgeContent={favoritos.length} color="success">
-                Mis favoritos
-              </Badge>
-            </Link>
-            
+            {user ? (
+              <Link className="nav-link" to="/favoritos">
+                <Badge badgeContent={favoritos.length} color="success">
+                  Mis favoritos
+                </Badge>
+              </Link>
+            ) : null}
           </Nav>
           <Nav>
-            <li className="nav-item" onClick={() => {salir()}}>
+            {/* <li className="nav-item" onClick={() => {salir()}}>
               Salir
-            </li>
+            </li> */}
+            {user ? (
+              <NavDropdown
+                title={
+                  <div className="d-inline">
+                    <img
+                      src={user.photoURL}
+                      className="me-2"
+                      style={{borderRadius: "50%", width: "30px"}}
+                      alt="foto usuario"
+                    />
+                    <span>{user.displayName}</span>
+                  </div>
+                }
+              >
+                <NavDropdown.Item onClick={salir}>Salir</NavDropdown.Item>
+              </NavDropdown>
+            ):(
+              <Link className="nav-link" to="/login">Ingresa</Link>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
