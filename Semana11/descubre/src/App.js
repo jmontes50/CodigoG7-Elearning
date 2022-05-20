6a18334a0f95e56c62b87f45abbdb3cbb6116289
Osmar Montesinos <osmarmontes50@gmail.com>
2022-05-19 20:17:58 -0500

    update

--- Semana11/descubre/src/App.js
+++ Semana11/descubre/src/App.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import FavoritosContextProvider from "./context/favoritosContext"
+import { AuthContextProvider } from "./context/authContext"
 import BarNavigation from "./components/BarNavigation"
 import HomeView from "./views/HomeView"
 import DetalleLugarView from "./views/DetalleLugarView"
@@ -9,16 +10,18 @@ import FavoritosView from "./views/FavoritosView"
 export default function App() {
   return (
     <Router>
-      <FavoritosContextProvider>
-        <BarNavigation />
-        <div className="container mt-4">
-          <Routes>
-            <Route path="/" element={<HomeView />} />
-            <Route path="/detallelugar/:catId/:lugId" element={<DetalleLugarView />} />
-            <Route path="/favoritos" element={<FavoritosView />} />
-          </Routes>
-        </div>
-      </FavoritosContextProvider>
+      <AuthContextProvider>
+        <FavoritosContextProvider>
+          <BarNavigation />
+          <div className="container mt-4">
+            <Routes>
+              <Route path="/" element={<HomeView />} />
+              <Route path="/detallelugar/:catId/:lugId" element={<DetalleLugarView />} />
+              <Route path="/favoritos" element={<FavoritosView />} />
+            </Routes>
+          </div>
+        </FavoritosContextProvider>
+      </AuthContextProvider>
     </Router>
   )
 }
--- Semana11/descubre/src/context/authContext.js
+++ Semana11/descubre/src/context/authContext.js
@@ -1,17 +1,17 @@
 import { useState, useEffect, createContext } from "react";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth"; //para conectarnos a google
-import { auth } from "../config/firebaseConfig";
+import { auth } from "../config/firebaseConfig"; //instancia del módulo auth de firebase
 
 export const AuthContext = createContext();
 
-const proveedorGoogle = new GoogleAuthProvider()
+const proveedorGoogle = new GoogleAuthProvider() //proveedor de google para identificar el método de acceso
 
 export const AuthContextProvider = (props) => {
     const [user, setUser] = useState(null)
 
     const signInGoogle = async () => {
         try {
-            const resultado = await signInWithPopup(auth, proveedorGoogle)
+            const resultado = await signInWithPopup(auth, proveedorGoogle) //loguearse con popup
             console.log({resultado})
         } catch (error) {
             console.log({error})
