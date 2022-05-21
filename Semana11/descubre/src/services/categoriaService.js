cd1411f43831acb58558ab045539054e29e1c33d
Osmar Montesinos <osmarmontes50@gmail.com>
2022-05-20 20:19:54 -0500

    update

--- Semana11/descubre/src/services/categoriaService.js
+++ Semana11/descubre/src/services/categoriaService.js
@@ -13,6 +13,17 @@ const obtenerCategorias = async (page = 1, limit = 10) => {
     }
 }
 
+const obtenerCategoriaPorId = async (id) => {
+    try {
+        const { data, status } = await axios.get(`${URL}/categorias/${id}`)
+        if( status === 200) return data
+        throw Error("Error al obtener data de categoria")
+    } catch (error) {
+        throw error
+    }
+}
+
 export {
-    obtenerCategorias
+    obtenerCategorias,
+    obtenerCategoriaPorId
 }
