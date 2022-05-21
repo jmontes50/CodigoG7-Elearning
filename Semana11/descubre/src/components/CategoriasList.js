60b3f49f22dab0e9e713aaaca5588d55d88f9858
Osmar Montesinos <osmarmontes50@gmail.com>
2022-05-20 19:46:12 -0500

    update

--- Semana11/descubre/src/components/CategoriasList.js
+++ Semana11/descubre/src/components/CategoriasList.js
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
 import { obtenerCategorias } from "../services/categoriaService";
+import { Link } from "react-router-dom";
 
 import LugaresList from "./LugaresList";
 
 export default function CategoriasList() {
   const [misCategorias, setMisCategorias] = useState([]);
   const [page, setPage] = useState(1);
-  const [limit, setLimit] = useState(10);
+  const [limit, setLimit] = useState(4);
 
   const getCategorias = async () => {
     try {
@@ -27,11 +28,24 @@ export default function CategoriasList() {
 
   return (
     <div>
-      {misCategorias.map(({ cat_nom }, i) => (
-        <button className="btn btn-success btn-sm me-2" key={i}>
-          {cat_nom}
-        </button>
-      ))}
+      <div className="row">
+        {misCategorias.map(({ cat_nom, cat_img }, i) => (
+          // <button className="btn btn-success btn-sm me-2" key={i}>
+          //   {cat_nom}
+          // </button>
+          <div className="col-12 col-md-6" key={i}>
+            <Link 
+              className="card mb-3 card-categoria" 
+              style={{backgroundImage:`url(${cat_img})`}}
+              to="/"
+            >
+              <div className="card-body">
+                <h4 className="card-title">{cat_nom}</h4>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
       <button className="btn btn-outline-success btn-sm" onClick={manejarVerMas}>
         Ver más...
       </button>
