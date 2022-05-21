6f430e2e69c7b8a54a7fbfbf176b5d7ec22a08e8
Osmar Montesinos <osmarmontes50@gmail.com>
2022-05-20 20:59:58 -0500

    update

--- Semana11/descubre/src/views/CategoriaView.js
+++ Semana11/descubre/src/views/CategoriaView.js
@@ -19,10 +19,44 @@ export default function CategoriaView() {
     getCategoria()
   },[])
 
-  return (
-    <div className="row">
+  if(!categoria) {
+    //poner algo de carga, como mi estado de categoria es null, evitar tener errores al revisar propiedades como lug_nom
+    return <h4>Cargando...</h4>
+  }
 
-        
+  return (
+    <div className="row">     
+      <h1 className="mb-4">
+        {categoria.cat_nom}
+      </h1>
+      <div className="col-12 col-md-4">
+        Categorias y Filtros Temp
+      </div>
+      <div className="col-12 col-md-8">
+        <h4 className="mb-3">Lugares recomendados</h4>
+        <div className="row">
+          {categoria.lugares.map(({lug_nom, lug_dir, lug_desc, lug_img, lug_id, categoriaId}, i) => (
+            <div className="col-12 col-md-4" key={i}>
+              <div className="card mb-3">
+                <img src={lug_img} className="card-img-top" alt={lug_nom} />
+                <div className="card-body">
+                  <h5 className="card-title">{lug_nom}</h5>
+                  <p className="card-text">
+                    {lug_desc}
+                  </p>
+                  <p className="card-text">
+                    <i className="fa-solid fa-location-dot text-success me-2" />
+                    {lug_dir}
+                  </p>
+                  <Link className="btn btn-success" to={`/detallelugar/${categoriaId}/${lug_id}`}>
+                    Conocer Lugar
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
