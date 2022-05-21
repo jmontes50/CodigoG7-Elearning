74247e05c82615263a3f12ea71c72f9a10af9a3d
Osmar Montesinos <osmarmontes50@gmail.com>
2022-05-20 19:22:55 -0500

    update

--- Semana11/descubre/src/App.js
+++ Semana11/descubre/src/App.js
@@ -1,4 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import "./styles.css"
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import FavoritosContextProvider from "./context/favoritosContext"
 import { AuthContextProvider } from "./context/authContext"
