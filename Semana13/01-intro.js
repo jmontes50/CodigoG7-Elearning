d78777151699234b3e7f8b70ed76a80f25e36650
Osmar Montesinos <osmarmontes50@gmail.com>
2022-05-25 19:37:26 -0500

    update

---
+++ Semana13/01-intro.js
@@ -0,0 +1,9 @@
+var saludo = "Hola Mundo!";
+console.log(saludo);
+//Tipado
+//let nom_variable:tipo = valor
+var texto = "ceviche";
+var cantidad = 21;
+var teQuiere = true;
+var regalos = ["Flores", "Chocolates", "Osito", "PS5"];
+var random = undefined; //básicamente cualquier cosa
---
+++ Semana13/01-intro.ts
@@ -0,0 +1,16 @@
+let saludo = "Hola Mundo!";
+
+console.log(saludo);
+
+//Tipado
+//let nom_variable:tipo = valor
+let texto:string = "ceviche";
+
+let cantidad:number = 21
+
+let teQuiere:boolean = true;
+
+let regalos:Array<string> = ["Flores","Chocolates","Osito","PS5"];
+
+let random:any = undefined //básicamente cualquier cosa
+
