009f247305240746fb7186583bfec50710fc4daa
Osmar Montesinos <osmarmontes50@gmail.com>
2022-05-25 23:01:31 -0500

    update

---
+++ Semana13/introangular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+
+@NgModule({
+  //DECLARATIONS: Componentes
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  //IMPORTS: Modulos
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  //PROVIDERS: servicios
+  providers: [],
+  //BOOTSTRAP: porque componente va a iniciar angular
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
