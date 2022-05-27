3b1d341daf169348fe060014b8dedc892b3763fe
Osmar Montesinos <osmarmontes50@gmail.com>
2022-05-26 22:24:41 -0500

    update

--- Semana13/nglugares/src/app/components/lugares/lugares.component.ts
+++ Semana13/nglugares/src/app/components/lugares/lugares.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LugaresService } from '../../services/lugares.service';
+import { Subscription } from 'rxjs'; //type, tipo
 
 @Component({
   selector: 'app-lugares',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lugares.component.css']
 })
 export class LugaresComponent implements OnInit {
+  suscripcionLugares:Subscription
 
-  constructor() { }
+  //inyección por dependencias
+  constructor(private _sLugares: LugaresService) { }
+
+  getLugares() {
+    // .subscribe(() => {exito}, () => {error})
+    this.suscripcionLugares = this._sLugares.obtenerCategorias()
+    .subscribe((datos) => {
+      console.log(datos)
+    })
+  }
 
   ngOnInit(): void {
+    //todo lo que sea consumo de servicios va a ir aquí
   }
 
 }
