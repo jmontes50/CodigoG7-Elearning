dfbea6e9ca380f3507eb7f752f4f6cdad52c149d
Osmar Montesinos <osmarmontes50@gmail.com>
2022-05-27 21:33:05 -0500

    update

--- Semana13/nglugares/src/app/services/lugares.service.ts
+++ Semana13/nglugares/src/app/services/lugares.service.ts
@@ -3,18 +3,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http"
 //Observable es la manera que se van a manejar el resultado de mis peticiones, types
 import { Observable } from 'rxjs';
+import iLugar from '../interfaces/iLugar';
+
 @Injectable({
   //que no necesito ponerlo en el app.module.ts
   providedIn: 'root'
 })
 export class LugaresService {
-  url:string = "https://62729d5925fed8fcb5f724b1.mockapi.io/categorias"
+  url:string = "https://62729d5925fed8fcb5f724b1.mockapi.io"
 
   //inyección por dependencias, declarar algo como una propiedad más
   // reemplazo const _Http = new HttpClient()
   constructor(private _Http: HttpClient) { }
 
   obtenerCategorias(): Observable<any> {
-    return this._Http.get(this.url)
+    return this._Http.get(`${this.url}/categorias`)
+  }
+
+  crearNuevoLugar(nuevoLugar:iLugar):Observable<any> {
+    return this._Http.post(`${this.url}/lugares`, nuevoLugar)
   }
 }
