af479c60e90785cb45194d3df5d478e5cdcd6456
Osmar Montesinos <osmarmontes50@gmail.com>
2022-05-27 22:37:16 -0500

    update

--- Semana13/nglugares/src/app/app-routing.module.ts
+++ Semana13/nglugares/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LugaresComponent } from './components/lugares/lugares.component';
 import { CrearlugarComponent } from './components/crearlugar/crearlugar.component';
+import { EditarlugarComponent } from './components/editarlugar/editarlugar.component';
 
 //routes va a ser un arreglo donde cada elemento va a representar 01 ruta
 //{path:"ruta", component:NombreComponente}
 const routes: Routes = [
   { path: '', component: LugaresComponent },
-  { path: 'crearlugar', component: CrearlugarComponent }
+  { path: 'crearlugar', component: CrearlugarComponent },
+  { path: 'editarlugar/:catId/:lugId', component: EditarlugarComponent },
 ];
 
 @NgModule({
